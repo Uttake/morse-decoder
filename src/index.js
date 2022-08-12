8fc74dd1fc3590fc1738a240d5fb0271e238ab95
Uttake <kazimirov.anton@mail.ru>
2022-08-12 21:50:58 +0300

    feat: solutions

--- src/index.js
+++ src/index.js
@@ -37,8 +37,34 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
+function decode(put) {
+    let result = []
+    let splitArr = []
+    for(let i = 0; i < put.length; i++) {
+        if(i % 10 === 0) {
+            splitArr.push([])
+        }
+    splitArr[splitArr.length - 1].push(put[i])
+    } 
+    for(let i = 0; i < splitArr.length; i++) {
+     result[i] = decoder(splitArr[i].join(''))
+    }
+    return result.join('')
+}
+
+function decoder(put) {
+    if(put === '**********') {
+        return ' '
+    }
+    let dots = put.replace(/10/g, ".")
+    let doshs = dots.replace(/11/g, "-")
+    let result = []
+    for(let i = 0; i < doshs.length; i++) {
+        if(doshs[i] !== '0') {
+            result += doshs[i]
+        }
+    }
+    return MORSE_TABLE[result]
 }
 
 module.exports = {
